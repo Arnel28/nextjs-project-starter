c84f12f7cf3e9816c90638ecd4ebb7ffb70c4f3e
Arnel28 <101615392+Arnel28@users.noreply.github.com>
2025-08-07 09:34:26 +0800

    Add .next/static/chunks/src_app_quiz_[id]_page_tsx_a993527f._.js 1754530465273

---
+++ .next/static/chunks/src_app_quiz_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_lib_security_lockdown_ts_c5b762a0._.js",
+  "static/chunks/src_11143365._.js",
+  "static/chunks/node_modules_f655ae8d._.js"
+],
+    source: "dynamic"
+});
